87ccf793f201b35e7e929a1016d6050867b4d63c
Lars Bilke <lars.bilke@ufz.de>
2018-08-03 09:31:39 +0200

    drop: build only release

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import copy
 import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs = ["x86_64"])
+    builder = ConanMultiPackager(archs = ["x86_64"], build_types = ["Release"])
     builder.add_common_builds(pure_c=False)
     items = []
     for item in builder.items:
