f7e3ab2fd32c0e77f23a93a7cd3d492a14a63982
Luis Díaz Más <piponazo@gmail.com>
2018-09-06 15:59:53 +0200

    Remove so many options

--- build.py
+++ build.py
@@ -5,18 +5,4 @@ import platform
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs = ["x86_64"], build_types = ["Release"])
     builder.add_common_builds(pure_c=False)
-    items = []
-    for item in builder.items:
-        if item.settings["compiler"] == "Visual Studio":
-            if item.settings["compiler.runtime"] == "MT" or item.settings["compiler.runtime"] == "MTd":
-                # Ignore MT runtime
-                continue
-        new_options = copy.copy(item.options)
-        new_options["VTK:qt"] = True
-        items.append([item.settings, new_options, item.env_vars, item.build_requires])
-        new_options = copy.copy(item.options)
-        new_options["VTK:minimal"] = True
-        new_options["VTK:ioxml"] = True
-        items.append([item.settings, new_options, item.env_vars, item.build_requires])
-    builder.items = items
     builder.run()
