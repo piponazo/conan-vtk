58ca5817885c104e9138e98ffaa9c90434358d5b
Luis Díaz Más <piponazo@gmail.com>
2018-09-06 17:41:40 +0200

    Only build 1 package with default options. Otherwise travis jobs expires

--- build.py
+++ build.py
@@ -4,5 +4,5 @@ import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs = ["x86_64"], build_types = ["Release"])
-    builder.add_common_builds(pure_c=False)
+    builder.add(settings={"arch": "x86_64", "build_type": "Release"})
     builder.run()
